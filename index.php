e9c694c542be1f9572f0eee7a8f7000412940d2b
pin <devinperdana93@gmail.com>
2026-06-17 17:16:52 +0700

    Style index.php

--- index.php
+++ index.php
@@ -2,61 +2,63 @@
 session_start();
 include "koneksi.php";
 
-
 if (isset($_POST['login'])) {
-
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    $query = mysqli_query(
-        $conn,
-        "SELECT * FROM users
-        WHERE username='$username'
-        AND password='$password'"
-    );
+    $query = mysqli_query($conn, "SELECT * FROM users WHERE username='$username' AND password='$password'");
 
     if (mysqli_num_rows($query) > 0) {
-
-        $_SESSION['login'] = true;
+        $_SESSION['login']    = true;
         $_SESSION['username'] = $username;
-
         header("Location: dashboard.php");
         exit;
     } else {
-        $error = "Username atau Password salah";
+        $error = "Username atau password salah.";
     }
 }
 ?>
 
 <!DOCTYPE html>
-<html>
-
+<html lang="id">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link rel="stylesheet" href="style.css">
 </head>
-
 <body>
 
-    <h2>Login</h2>
+<div class="login-wrapper">
+    <div class="login-box">
 
-    <?php
-    if (isset($error)) {
-        echo $error;
-    }
-    ?>
+        <h2>Login</h2>
+        <p>Aplikasi Manajemen Keuangan</p>
 
-    <form method="POST">
+        <?php if (isset($error)) { ?>
+            <div class="error"><?= $error; ?></div>
+        <?php } ?>
 
-        <label>Username</label><br>
-        <input type="text" name="username"><br><br>
+        <form method="POST">
 
-        <label>Password</label><br>
-        <input type="password" name="password"><br><br>
+            <div class="form-group" style="text-align: left;">
+                <label>Username</label>
+                <input type="text" name="username" placeholder="Masukkan username">
+            </div>
 
-        <button name="login">Login</button>
+            <div class="form-group" style="text-align: left;">
+                <label>Password</label>
+                <input type="password" name="password" placeholder="Masukkan password">
+            </div>
 
-    </form>
+            <button name="login" type="submit" class="btn btn-biru" style="width: 100%; padding: 10px;">
+                Login
+            </button>
+
+        </form>
+
+    </div>
+</div>
 
 </body>
-
 </html>
