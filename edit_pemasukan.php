60e68df109c53ca528f071df6e7fcf45657dfc7c
pin <devinperdana93@gmail.com>
2026-06-17 17:22:07 +0700

    Style edit_pemasukan.php

--- edit_pemasukan.php
+++ edit_pemasukan.php
@@ -1,49 +1,78 @@
 <?php
+session_start();
+if (!isset($_SESSION['login'])) {
+    header("Location: index.php");
+    exit;
+}
 
 include "koneksi.php";
 
-$id = $_GET['id'];
-
-$data = mysqli_fetch_assoc(
-    mysqli_query(
-        $conn,
-        "SELECT * FROM pemasukan
-        WHERE id='$id'"
-    )
-);
+$id   = $_GET['id'];
+$data = mysqli_fetch_assoc(mysqli_query($conn, "SELECT * FROM pemasukan WHERE id='$id'"));
 
 if (isset($_POST['update'])) {
-
-    $tanggal = $_POST['tanggal'];
+    $tanggal    = $_POST['tanggal'];
     $keterangan = $_POST['keterangan'];
-    $jumlah = $_POST['jumlah'];
+    $jumlah     = $_POST['jumlah'];
 
-    mysqli_query(
-        $conn,
-        "UPDATE pemasukan SET
-        tanggal='$tanggal',
-        keterangan='$keterangan',
-        jumlah='$jumlah'
-        WHERE id='$id'"
-    );
+    mysqli_query($conn, "UPDATE pemasukan SET tanggal='$tanggal', keterangan='$keterangan',
+                         jumlah='$jumlah' WHERE id='$id'");
 
     header("Location: pemasukan.php");
+    exit;
 }
 ?>
 
-<form method="POST">
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edit Pemasukan</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
 
-    Tanggal<br>
-    <input type="date" name="tanggal" value="<?= $data['tanggal']; ?>"><br><br>
+<nav class="navbar">
+    <div class="navbar-inner">
+        <span class="navbar-brand">Manajemen Keuangan</span>
+        <div class="navbar-links">
+            <a href="dashboard.php">Dashboard</a>
+            <a href="pemasukan.php" class="aktif">Pemasukan</a>
+            <a href="pengeluaran.php">Pengeluaran</a>
+            <a href="logout.php" class="keluar">Logout</a>
+        </div>
+    </div>
+</nav>
 
-    Keterangan<br>
-    <input type="text" name="keterangan" value="<?= $data['keterangan']; ?>"><br><br>
+<div class="container">
 
-    Jumlah<br>
-    <input type="number" name="jumlah" value="<?= $data['jumlah']; ?>"><br><br>
+    <h2>Edit Pemasukan</h2>
 
-    <button name="update">
-        Update
-    </button>
+    <form method="POST" style="max-width: 450px;">
 
-</form>
+        <div class="form-group">
+            <label>Tanggal</label>
+            <input type="date" name="tanggal" value="<?= $data['tanggal']; ?>" required>
+        </div>
+
+        <div class="form-group">
+            <label>Keterangan</label>
+            <input type="text" name="keterangan" value="<?= htmlspecialchars($data['keterangan']); ?>" required>
+        </div>
+
+        <div class="form-group">
+            <label>Jumlah (Rp)</label>
+            <input type="number" name="jumlah" value="<?= $data['jumlah']; ?>" required>
+        </div>
+
+        <button name="update" type="submit" class="btn btn-hijau">Update</button>
+        &nbsp;
+        <a href="pemasukan.php" class="btn btn-abu">Batal</a>
+
+    </form>
+
+</div>
+
+</body>
+</html>
