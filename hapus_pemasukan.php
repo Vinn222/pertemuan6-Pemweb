184b32c0e64896504edf9f4ccdc214db202d7c26
pin <devinperdana93@gmail.com>
2026-06-18 14:47:42 +0700

    edit hapus pemasukan dan pengeluaran

--- hapus_pemasukan.php
+++ hapus_pemasukan.php
@@ -1,13 +1,8 @@
 <?php
-
 include "koneksi.php";
 
 $id = $_GET['id'];
 
-mysqli_query(
-    $conn,
-    "DELETE FROM pemasukan
-    WHERE id='$id'"
-);
+mysqli_query($conn, "DELETE FROM pemasukan WHERE id='$id'");
 
 header("Location: pemasukan.php");
--- hapus_pengeluaran.php
+++ hapus_pengeluaran.php
@@ -1,13 +1,8 @@
 <?php
-
 include "koneksi.php";
 
 $id = $_GET['id'];
 
-mysqli_query(
-    $conn,
-    "DELETE FROM pengeluaran
-    WHERE id='$id'"
-);
+mysqli_query($conn, "DELETE FROM pengeluaran WHERE id='$id'");
 
 header("Location: pengeluaran.php");
