efb970d32d6b18721261297fa8b36c36556f1e7e
pin <devinperdana93@gmail.com>
2026-06-21 17:30:11 +0700

    Update struktur folder dan koneksi database aiven

---
+++ api/koneksi.php
@@ -0,0 +1,15 @@
+<?php
+// Mengambil data dari Environment Variables Vercel yang sudah Anda isi tadi
+$host = getenv('DB_HOST');
+$port = getenv('DB_PORT');
+$user = getenv('DB_USER');
+$pass = getenv('DB_PASSWORD');
+$db   = getenv('DB_NAME');
+
+// Menghubungkan ke MySQL Aiven dengan Port khusus
+$conn = mysqli_connect($host, $user, $pass, $db, $port);
+
+if (!$conn) {
+    die("Koneksi gagal : " . mysqli_connect_error());
+}
+?>
